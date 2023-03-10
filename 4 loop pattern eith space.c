c6c23026a966180bc074524ccce1205d3c9010ba
jiyasavaliya18 <126229948+jiyasavaliya18@users.noreply.github.com>
2023-03-10 17:19:12 +0530

    Add files via upload

---
+++ 4 loop pattern eith space.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+  int r,c,s;
+  for(r=5; r>=1; r--)
+  {
+    for(s=1; s<r; s++)
+      printf(" ");
+    for(c=5; c>=r; c--)
+      printf("%d",c);
+    printf("\n");
+  }
+  return 0;
+}
+   
+
