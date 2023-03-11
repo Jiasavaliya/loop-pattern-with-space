78e273f8af9c76332174b8133691257bcc7e6eae
jiyasavaliya18 <126229948+jiyasavaliya18@users.noreply.github.com>
2023-03-11 13:17:21 +0530

    Add files via upload

---
+++ 9 loop pattern with space.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+main(){
+	int r,c,s;
+	
+	for(r=1; r<=5; r++){
+		
+	for(s=r; s>1; s--)
+			printf(" ");
+	for(c=5; c>=r; c--){
+		
+	
+		printf("%d",c);
+	}
+	printf("\n");
+}
+}
+
+
